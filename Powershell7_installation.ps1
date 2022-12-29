3af030ed3f16bcda9a42803ba9ea4f5bc4491993
rohitnarra <rohit.narra123@gmail.com>
2022-12-29 18:38:36 +0530

    Powershell repo

---
+++ Powershell7_installation.ps1
@@ -0,0 +1,6 @@
+﻿mkdir "C:\PowershellScripts"
+Set-Location C:\PowershellScripts
+$URI = "https://aka.ms/powershell-installation.ps1"
+Invoke-RestMethod -Uri https://aka.ms/powershell-installation.ps1 | 
+Out-File -FilePath C:\PowershellScripts\powershell-installation.ps1
+
---
+++ Setup_configuration.ps1
@@ -0,0 +1,9 @@
+﻿Get-Help -Name C:\PowershellScripts\powershell_installation.ps1
+
+$PARAMS = @{
+ UseMsi = $true
+ Quiet = $true
+ AddExplorerContextMenu = $true
+ EnablePSRemoting = $true
+}
+C:\PowershellScripts\powershell_installation.ps1 @PARAMS
